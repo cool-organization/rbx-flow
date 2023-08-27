ff00e7d57db61010fb02c4be09cea9cc392955e0
howmanysmall <lhamil29@bruinmail.slcc.edu>
2023-08-27 16:44:20 -0600

    export type

--- src/index.d.ts
+++ src/index.d.ts
@@ -21,31 +21,60 @@ import type { Size, Value } from "./utils";
 
 declare namespace Flow {
 	export const Align: typeof YGAlign;
+	export type Align = YGAlign;
+
 	export const Dimension: typeof YGDimension;
+	export type Dimension = YGDimension;
+
 	export const Direction: typeof YGDirection;
+	export type Direction = YGDirection;
+
 	export const Display: typeof YGDisplay;
+	export type Display = YGDisplay;
+
 	export const Edge: typeof YGEdge;
+	export type Edge = YGEdge;
+
 	export const ExperimentalFeature: typeof YGExperimentalFeature;
+	export type ExperimentalFeature = YGExperimentalFeature;
+
 	export const FlexDirection: typeof YGFlexDirection;
+	export type FlexDirection = YGFlexDirection;
+
 	export const Justify: typeof YGJustify;
+	export type Justify = YGJustify;
+
 	export const LogLevel: typeof YGLogLevel;
+	export type LogLevel = YGLogLevel;
+
 	export const MeasureMode: typeof YGMeasureMode;
+	export type MeasureMode = YGMeasureMode;
+
 	export const NodeType: typeof YGNodeType;
+	export type NodeType = YGNodeType;
+
 	export const Overflow: typeof YGOverflow;
+	export type Overflow = YGOverflow;
+
 	export const PositionType: typeof YGPositionType;
+	export type PositionType = YGPositionType;
+
 	export const Unit: typeof YGUnit;
+	export type Unit = YGUnit;
+
 	export const Wrap: typeof YGWrap;
+	export type Wrap = YGWrap;
 
 	export type FlowConfig = Config;
 	export type FlowNode = Node;
 	export type FlowSize = Size;
-	export type FlowUnit = YGUnit;
+	export type FlowValue = Value;
 
-	export const Config: new () => Config;
-	export const Node: new (config?: Config) => Node;
+	export const Config: new () => FlowConfig;
+	export const Node: new (config?: FlowConfig) => FlowNode;
 
-	export const Size: (new (width: number, height?: number) => Size) | (new () => Size);
-	export const Value: (new (unit: YGUnit, value?: number) => Value) | (new () => Value);
+	export const Size: (new (width: number, height?: number) => FlowSize) | (new () => FlowSize);
+	export const Value: (new (unit: YGUnit, value?: number) => FlowValue) | (new () => FlowValue);
 }
 
 export = Flow;
